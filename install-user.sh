37bb7137f3fbc403584bf98a7a9be1762a30d2e0
Igor Epstein <igorepst@gmail.com>
2020-06-28 23:15:52 +0300

    Cont.

--- install-user.sh
+++ install-user.sh
@@ -1,34 +1,44 @@
 #!/usr/bin/env bash
 
-{
+function doWork(){
+    export RED=$(tput setaf 1)
+    export GREEN=$(tput setaf 2)
+    export RESET=$(tput sgr0)
+
+    echo ${GREEN}'Installing common configuration'${RESET}
+    echo
+
     DOTFILES_DIR="$(
     cd "$(dirname "$0")"
     pwd
     )"
     export DOTFILES_DIR
-    pushd "$DOTFILES_DIR" >/dev/null
+    pushd "${DOTFILES_DIR}" >/dev/null
 
-    local MY_PC_ANSWER_FILE="$DOTFILES_DIR/my_pc_is"
-    [ -f $MY_PC_ANSWER_FILE ] && source $MY_PC_ANSWER_FILE
-    if [ -z $MY_PC_IS ]; then
+    local MY_PC_ANSWER_FILE="${DOTFILES_DIR}/my_pc_is"
+    [ -f ${MY_PC_ANSWER_FILE} ] && source ${MY_PC_ANSWER_FILE}
+    if [ -z ${MY_PC_IS} ]; then
         local MY_PC_IS_H
+        echo ${RED}'Checking environment'${RESET}
         read -p "Is it a home PC? [Y]es:[N]o " MY_PC_IS_H
-        if [[ "$MY_PC_IS_H" =~ ^[Yy]$ ]]; then
+        if [[ "${MY_PC_IS_H}" =~ ^[Yy]$ ]]; then
             MY_PC_IS="home"
         else
             MY_PC_IS="work"	
         fi
-        echo 'export MY_PC_IS='$MY_PC_IS>$MY_PC_ANSWER_FILE
+        echo 'export MY_PC_IS='${MY_PC_IS}>${MY_PC_ANSWER_FILE}
     fi
     export MY_PC_IS
 
 
-    local recipes_dir="$DOTFILES_DIR/installRecipes"
-    "$recipes_dir/01.packages.sh"
-    "$recipes_dir/02.gitconfig-private.sh"
-    "$recipes_dir/03.fonts.sh"
-    "$recipes_dir/04.links.sh"
-    "$recipes_dir/99.else.sh"
+    local RECIPES_DIR="${DOTFILES_DIR}/installRecipes"
+    "${RECIPES_DIR}/01.packages.sh"
+    "${RECIPES_DIR}/02.gitconfig-private.sh"
+    "${RECIPES_DIR}/03.fonts.sh"
+    "${RECIPES_DIR}/04.links.sh"
+    "${RECIPES_DIR}/99.else.sh"
 
     popd >/dev/null
 }
+
+doWork
