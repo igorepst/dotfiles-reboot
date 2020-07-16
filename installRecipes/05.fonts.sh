584dec3dd18c979ec4e8e35fff12a375925cc8d8
Igor Epstein <igorepst@gmail.com>
2020-07-16 13:39:33 +0300

    Fix forcing OTF conversion

--- installRecipes/05.fonts.sh
+++ installRecipes/05.fonts.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 function doWork(){
-    local TMP_DIR=$(mktemp -d) 
     echo ${GREEN}'Installing fonts:'${RESET}
     echo
-    local FONTS_DIR=~/.local/share/fonts
+    local FONTS_DIR=${HOME}/.local/share/fonts
+    mkdir -p "${FONTS_DIR}"
     local REG_FONT="DejaVu Sans Mono Nerd Font Complete Mono"
     local CONV_NEEDED=0
     if [ ! -f "${FONTS_DIR}/${REG_FONT}.ttf" ] || [ -n "${DOT_UPDATE_FONTS}" ]; then 
         CONV_NEEDED=1
         echo "${GREEN}Installing ${REG_FONT}.ttf"${RESET}
-        curl -o "${TMP_DIR}/${REG_FONT}.ttf" -L -O \
+        curl -o "${FONTS_DIR}/${REG_FONT}.ttf" -L -O \
 https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
     else
         echo "${GREEN}${REG_FONT}.ttf is installed${RESET}"
@@ -19,7 +19,7 @@ https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/
     if [ ! -f "${FONTS_DIR}/${BOLD_FONT}.ttf" ] || [ -n "${DOT_UPDATE_FONTS}" ]; then 
         CONV_NEEDED=1
         echo "${GREEN}Installing ${BOLD_FONT}.ttf"${RESET}
-        curl -o "${TMP_DIR}/${BOLD_FONT}.ttf" -L -O \
+        curl -o "${FONTS_DIR}/${BOLD_FONT}.ttf" -L -O \
 https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Bold/complete/DejaVu%20Sans%20Mono%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
     else
         echo "${GREEN}${BOLD_FONT}.ttf is installed${RESET}"
@@ -28,7 +28,7 @@ https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/
     if [ "${CONV_NEEDED}" -eq "1" ] || [ ! -f "${FONTS_DIR}/${REG_FONT}.otf" ] || [ ! -f "${FONTS_DIR}/${BOLD_FONT}.otf" ]; then 
         echo ${GREEN}'Converting TTF to OTF for pango'${RESET}
         local CONV_SCRIPT=convertFonts.py
-        cat >"${TMP_DIR}/${CONV_SCRIPT}" <<"EOF"
+        cat >"${FONTS_DIR}/${CONV_SCRIPT}" <<"EOF"
 import fontforge
 import os
 fonts = [f for f in os.listdir('.') if f.endswith('.ttf')]
@@ -36,16 +36,14 @@ for font in fonts:
         f = fontforge.open(font)
         f.generate(font[:-3] + 'otf')
 EOF
-        pushd "${TMP_DIR}" >/dev/null
+        pushd "${FONTS_DIR}" >/dev/null
         python ./"${CONV_SCRIPT}" &>/dev/null
+        rm "./${CONV_SCRIPT}"
         popd >/dev/null
     else
         echo ${GREEN}'OTF fonts exist'${RESET}
     fi
 
-    mkdir -p "${FONTS_DIR}"
-    mv -f -t "${FONTS_DIR}" "${TMP_DIR}/"*.[to]tf 2>/dev/null
-    rm -rf "${TMP_DIR}"
     fc-cache
 
     local XFONTS=${HOME}/.theme/currentTheme.Xfonts
