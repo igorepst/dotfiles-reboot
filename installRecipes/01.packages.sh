a307070bf6c0063123930620e7be1a02ef08e241
Igor Epstein <igor.epstein@jetpatch.com>
2020-09-17 13:55:02 +0300

    Vifm

--- installRecipes/01.packages.sh
+++ installRecipes/01.packages.sh
@@ -15,23 +15,42 @@ checkP mpv
 checkP xclip
 checkP bat
 checkP fd
-checkP vifm
-checkP gvim
 checkP xterm
 checkP terminus-font
 checkP curl
 checkP xorg-xrdb
 checkP tmux
 checkP rofi
+
 # Convert TTF to OTF for pango
 checkP fontforge
 checkP python
+
 # For AwesomeWM
 checkP awesome
 checkP gnome-keyring
 checkP libsecret
+
+checkP gvim
 # For coc.nvim
 checkP nodejs
+
+checkP vifm
+# For atool
+checkP atool
+checkP rpm-tools
+checkP bzip2
+checkP cpio
+checkP gzip
+checkP lha
+checkP xz
+checkP lzop
+checkP p7zip
+checkP tar
+checkP unace
+checkP unrar
+checkP zip
+checkP unzip
 
 if [ ! -z "${pnames}" ]; then
     echo ${RED}'Installing the following packages:'${RESET}
