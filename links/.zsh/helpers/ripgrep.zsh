53e2143d2faca1a7b3b9785538c65940a19470c3
Igor Epstein <igorepst@gmail.com>
2021-03-12 18:37:03 +0200

    Add rf()=rg+fzf

--- links/.zsh/helpers/ripgrep.zsh
+++ links/.zsh/helpers/ripgrep.zsh
@@ -12,3 +12,16 @@ function ri() {
     shift
     rg -e ${SVAL} --color=${COLORVAL} --smart-case --follow --heading --hidden --line-number --sort-files $@
 }
+
+function rf() {
+    local sel rg_pref initq
+    [ -n "$1" ] && initq="$1" || initq=''
+    rg_pref="rg --smart-case --follow --no-heading --line-number --color=never --trim --ignore-file ~/.config/ripgrep/ignore --hidden --sort-files"
+    sel=$(FZF_DEFAULT_COMMAND="$rg_pref -e '$initq'" fzf --query "$initq" --bind "change:reload:$rg_pref -e {q} || true" --disabled --info=inline --delimiter ':' --with-nth=3.. --preview 'a=$(expr {2} - 5); [ $a -lt 0 ] && a=0;((b=$a + 15));bat --color=always --line-range=$a:$b --highlight-line {2} {1}' --preview-window up)
+
+    if [ -n "$sel" ]; then
+        local ln fn tmp
+        IFS=':' read -r fn ln tmp <<<"$sel"
+        vim "$fn" +"$ln"
+    fi
+}
