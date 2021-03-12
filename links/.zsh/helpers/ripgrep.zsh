c401a9271c9d395b93c0196856193c54f029ead1
Igor Epstein <igorepst@gmail.com>
2021-03-12 19:09:47 +0200

    Push to history with rf()

--- links/.zsh/helpers/ripgrep.zsh
+++ links/.zsh/helpers/ripgrep.zsh
@@ -17,11 +17,13 @@ function rf() {
     local sel rg_pref initq
     [ -n "$1" ] && initq="$1" || initq=''
     rg_pref="rg --smart-case --follow --no-heading --line-number --color=never --trim --ignore-file ~/.config/ripgrep/ignore --hidden --sort-files"
-    sel=$(FZF_DEFAULT_COMMAND="$rg_pref -e '$initq'" fzf --query "$initq" --bind "change:reload:$rg_pref -e {q} || true" --disabled --info=inline --delimiter ':' --with-nth=3.. --preview 'a=$(expr {2} - 5); [ $a -lt 0 ] && a=0;((b=$a + 15));bat --color=always --line-range=$a:$b --highlight-line {2} {1}' --preview-window up)
+    sel=$(FZF_DEFAULT_COMMAND="$rg_pref -e '$initq'" fzf --query "$initq" --bind "change:reload:$rg_pref -e {q} || true" --disabled --info=inline --delimiter ':' --with-nth=3.. --preview 'a=$(expr {2} - 5);[ $a -lt 0 ] && a=0;((b=$a + 15));bat --color=always --line-range=$a:$b --highlight-line {2} {1}' --preview-window up)
 
     if [ -n "$sel" ]; then
         local ln fn tmp
         IFS=':' read -r fn ln tmp <<<"$sel"
+        # Add to history
+        print -s vim "$fn" +"$ln"
         vim "$fn" +"$ln"
     fi
 }
