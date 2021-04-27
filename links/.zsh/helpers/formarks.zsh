c7e2ad3aa8a639c783f977867c6ebe57f579ce1c
Igor Epstein <igor.epstein@jetpatch.com>
2021-04-27 12:01:21 +0300

    Update dots

--- links/.zsh/helpers/formarks.zsh
+++ links/.zsh/helpers/formarks.zsh
@@ -2,3 +2,13 @@
 export FZF_MARKS_JUMP='g'
 export PATHMARKS_FILE=~/.zsh/volatile/pathmarks
 source ~/.zsh/plugins/formarks/formarks.plugin.zsh
+
+function pathmarks-colorize-override() {
+    local field='\(\S\+\s*\)'
+    local esc=$(printf '\033')
+    local N="${esc}[0m"
+    local R="${esc}[31m"
+    sed "s#^${field}${field}${field}${field}#\1$R\2$N\3\4#"
+}
+
+functions[wfxr::pathmarks-colorize]=$functions[pathmarks-colorize-override]
