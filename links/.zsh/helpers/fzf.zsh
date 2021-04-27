c97a40f2cba0cf34660f033534f158a237cdb831
Igor Epstein <igor.epstein@jetpatch.com>
2021-04-27 11:32:26 +0300

    Update dots

--- links/.zsh/helpers/fzf.zsh
+++ links/.zsh/helpers/fzf.zsh
@@ -1,6 +1,6 @@
 source /usr/share/fzf/completion.zsh
 source /usr/share/fzf/key-bindings.zsh
 
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --reverse --color=fg:#1c1c1c,bg:#eeeeee,hl:#d70087,fg+:#1c1c1c,bg+:#e4e4e4,hl+:#d70087,info:#0087af,prompt:#005faf,pointer:#d70087,marker:#0087af,spinner:#0087af,header:#0087af"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --reverse --color=fg:#282828,bg:#fffffa,hl:1,fg+:#ffffff,bg+:#74b1d1,hl+:1,info:6,prompt:4,pointer:5,marker:6,spinner:6,header:6"
 
 alias f.='fd --hidden -t f . ~/dotfiles-reboot | fzf | xargs -I{} kitty @ launch --type=tab $EDITOR -f "{}"'
