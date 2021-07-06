65d0d776cad682ed95ef914ed008d381e99e6061
Igor Epstein <igorepst@gmail.com>
2021-07-06 23:28:06 +0300

    Add Picom

--- recipes/03.links.sh
+++ recipes/03.links.sh
@@ -38,6 +38,7 @@ function doWork(){
     link ".config/rofi"
     link ".config/nvim"
     link ".config/kitty"
+    link ".config/picom"
     link ".local/share/navi"
     link ".gitconfig"
     link ".gitignore-global"
