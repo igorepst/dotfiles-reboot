8f9df2d7043b5457a7ad01766031d276cf642c82
Igor Epstein <igor.epstein@jetpatch.com>
2021-08-23 14:47:22 +0300

    Various fixes

--- links/.config/nvim/init.lua
+++ links/.config/nvim/init.lua
@@ -1,7 +1,28 @@
 vim.cmd([[
-    " Save last cursor position at \ mark and open buffer at last cursor position
-    augroup LastCursorPos
-    autocmd!
-    autocmd BufReadPost * if @% !~# "\.git[\/\\]COMMIT_EDITMSG$" && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+    " https://gist.github.com/romainl/56f0c28ef953ffc157f36cc495947ab3
+    set grepprg=rg\ --vimgrep
+
+    function! Grep(...)
+	return system(join([&grepprg] + [expandcmd(join(a:000, ' '))], ' '))
+    endfunction
+
+    command! -nargs=+ -complete=file_in_path -bar Grep  cgetexpr Grep(<f-args>)
+    command! -nargs=+ -complete=file_in_path -bar LGrep lgetexpr Grep(<f-args>)
+
+    cnoreabbrev <expr> grep  (getcmdtype() ==# ':' && getcmdline() ==# 'grep')  ? 'Grep'  : 'grep'
+    cnoreabbrev <expr> lgrep (getcmdtype() ==# ':' && getcmdline() ==# 'lgrep') ? 'LGrep' : 'lgrep'
+
+    augroup ig
+	autocmd!
+	autocmd QuickFixCmdPost cgetexpr cwindow
+	autocmd QuickFixCmdPost lgetexpr lwindow
+
+        " Save last cursor position at \ mark and open buffer at last cursor position
+        autocmd BufReadPost * if @% !~# "\.git[\/\\]COMMIT_EDITMSG$" && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
     augroup end
+
+    " Map Q to q and Q! to q!
+    command! -bang Q q<bang>
+    " Don't use Ex mode, use Q for formatting.
+    nnoremap Q gq
 ]])
