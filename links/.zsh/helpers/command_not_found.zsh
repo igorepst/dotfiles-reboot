bb2908afe5a71fd92040488db4b12140895934d0
Igor Epstein <igorepst@gmail.com>
2021-11-08 17:31:02 +0200

    Fix command_not_found for Ubuntu

--- links/.zsh/helpers/command_not_found.zsh
+++ links/.zsh/helpers/command_not_found.zsh
@@ -1,15 +1,22 @@
 # http://zsh.sourceforge.net/Doc/Release/Command-Execution.html
 # https://github.com/falconindy/pkgfile/blob/master/extra/command-not-found.zsh
 function command_not_found_handler() {
-  local pkgs cmd="$1"
-
-  pkgs=(${(f)"$(pacman -Fx "$cmd".\* 2>/dev/null)"})
-  if [[ -n "$pkgs" ]]; then
-    printf '%s is not installed, but may be found in the following packages:\n' "$cmd"
-    printf '  %s\n' $pkgs[@]
-  else
-    printf 'zsh: command not found: %s\n' "$cmd"
-  fi 1>&2
-
-  return 127
+    local cmd="$1"
+    if [ -x /usr/bin/pacman ]; then
+        local pkgs
+        pkgs=(${(f)"$(pacman -F "$cmd" 2>/dev/null)"})
+        if [[ -n "$pkgs" ]]; then
+            printf '%s is not installed, but may be found in the following packages:\n' "$cmd"
+            printf '  %s\n' $pkgs[@]
+            return 127
+        fi
+    elif [ -x /usr/lib/command-not-found ]; then
+        /usr/lib/command-not-found -- "$cmd"
+        return 127
+    elif [ -x /usr/share/command-not-found/command-not-found ]; then
+        /usr/share/command-not-found/command-not-found -- "$cmd"
+        return 127
+    fi
+    printf "zsh: command not found: ${cmd}\n"
+    return 127
 }
