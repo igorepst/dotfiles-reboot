990fc92486c60e5192a942dbf4a2e024979a6247
igor.epstein <igorepst@gmail.com>
2022-06-14 22:51:40 +0300

    Various fixes

--- links/.zsh/helpers/command_not_found.zsh
+++ links/.zsh/helpers/command_not_found.zsh
@@ -1,15 +1,32 @@
 # http://zsh.sourceforge.net/Doc/Release/Command-Execution.html
-# https://github.com/falconindy/pkgfile/blob/master/extra/command-not-found.zsh
+# https://wiki.archlinux.org/title/Zsh#pacman_-F_%22command_not_found%22_handler
 function command_not_found_handler() {
     local cmd="$1"
     if [ -x /usr/bin/pacman ]; then
-        local pkgs
-        pkgs=(${(f)"$(pacman -F "$cmd" 2>/dev/null)"})
-        if [[ -n "$pkgs" ]]; then
-            printf '%s is not installed, but may be found in the following packages:\n' "$cmd"
-            printf '  %s\n' $pkgs[@]
-            return 127
-        fi
+	local purple='\e[1;35m' bright='\e[0;1m' green='\e[1;32m' reset='\e[0m'
+	printf 'zsh: command not found: %s\n' "$1"
+	local entries=(
+            ${(f)"$(/usr/bin/pacman -F --machinereadable -- "/usr/bin/$1")"}
+	)
+	if (( ${#entries[@]} ))
+	then
+            printf "${bright}$1${reset} may be found in the following packages:\n"
+            local pkg
+            for entry in "${entries[@]}"
+            do
+		# (repo package version file)
+		local fields=(
+                    ${(0)entry}
+		)
+		if [[ "$pkg" != "${fields[2]}" ]]
+		then
+                    printf "${purple}%s/${bright}%s ${green}%s${reset}\n" "${fields[1]}" "${fields[2]}" "${fields[3]}"
+		fi
+		printf '    /%s\n' "${fields[4]}"
+		pkg="${fields[2]}"
+            done
+	fi
+	return 127
     elif [ -x /usr/lib/command-not-found ]; then
         /usr/lib/command-not-found -- "$cmd"
         return 127
