748e23ea8d65594a64efb74ef8617ab5ee3ce9c4
Igor Epstein <igor.epstein@moovit.com>
2022-08-31 18:13:47 +0300

    Various fixes

--- packages/emacs/config/emacs/lisp/ig-eshell.el
+++ packages/emacs/config/emacs/lisp/ig-eshell.el
@@ -54,7 +54,11 @@
 
 (defun eshell/gps()
   "Git push."
-  (eshell-interactive-print (vc-git-push nil)))
+  (eshell-interactive-print
+   (with-temp-buffer
+     (and
+      (vc-git--out-ok "push" "origin" (ig-git-get-branch))
+      (buffer-substring-no-properties (point-min) (point-max))))))
 
 (defun ig-eshell-prompt()
   "Define custom Eshell prompt."
