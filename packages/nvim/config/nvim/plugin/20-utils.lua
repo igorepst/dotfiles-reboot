1111f25b67c799cfeb41026a5b1fff8f62e70108
igor.epstein <igorepst@gmail.com>
2022-11-13 22:04:57 +0200

    Drop nvim

--- packages/nvim/config/nvim/plugin/20-utils.lua
+++
@@ -1,7 +0,0 @@
-function ShowCurrentWordHighlight()
-    vim.cmd([[TSBufDisable highlight]])
-    for _, v in ipairs(vim.fn.synstack(vim.fn.line('.'), vim.fn.col('.'))) do
-        print(vim.fn.synIDattr(v, 'name') .. ' -> ' .. vim.fn.synIDattr(vim.fn.synIDtrans(v), 'name'))
-    end
-    vim.cmd([[TSBufEnable highlight]])
-end
